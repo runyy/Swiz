0cf8dfc10e5224416998aa51824eeb287af1528c
Exility <49131545+exilityyy@users.noreply.github.com>
2020-11-23 18:42:20 +0100

    Delete design.js

--- design.js
+++
@@ -1,1 +0,0 @@
-function webscroll(cname) {var id = cname.replace("#", "");const el = document.getElementById(id);el.scrollIntoView({behavior: "smooth", inline: "nearest"});history.pushState(null, null, `#${id}`)}
